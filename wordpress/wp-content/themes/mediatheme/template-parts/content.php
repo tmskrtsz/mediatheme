ad52232721ea8ba4783e2108277c7c92ed3ae6e3
tmskrtsz <tamas.kertesz@cult.tamk.fi>
2017-12-02 19:21:22 +0200

    Add post type icons

--- wordpress/wp-content/themes/mediatheme/template-parts/content.php
+++ wordpress/wp-content/themes/mediatheme/template-parts/content.php
@@ -7,20 +7,34 @@
 
  ?>
 <div <?php post_class('col gallery') ?>>
-	<?php if ( has_post_thumbnail() ): ?>
-		<a class="gallery-thumbnail" href="<?php echo esc_url( get_permalink($post->ID) ) ?>">
-			<?php the_post_thumbnail('custom-image-thumb'); ?>
-		</a>
-	<?php else: ?>
-		<a class="gallery-thumbnail" href="<?php echo esc_url( get_permalink($post->ID) ) ?>">
+	<a class="gallery-thumbnail" href="<?php echo esc_url( get_permalink($post->ID) ) ?>">
+		<?php if ( !has_post_thumbnail() ) : ?>
 			<div class="gallery-thumbnail-gradient" style="<?php echo get_gradient(); ?>"></div>
-		</a>
-	<?php endif; ?>
+		<?php else:
+			the_post_thumbnail('custom-image-thumb');
+		endif;
+
+		if ( has_post_format('video') ): ?>
+			<div class="gallery-type">
+				<?php get_template_part( 'dist/images/inline', 'video.svg' ); ?>
+			</div>
+
+		<?php elseif ( has_post_format('chat') ): ?>
+			<div class="gallery-type">
+				<?php get_template_part( 'dist/images/inline', 'message-circle.svg' ); ?>
+			</div>
+
+		<?php elseif ( has_post_format('gallery') ): ?>
+			<div class="gallery-type post-format-gallery">
+				<?php get_template_part( 'dist/images/inline', 'image.svg' ); ?>
+			</div>
+		<?php endif; ?>
+	</a>
 	<div class="gallery-meta">
 		<div class="gallery-category">
 			<?php add_categories(1); ?>
 		</div>
-		<?php the_title( '<h2 class="gallery-title"><a href="' . 
+		<?php the_title( '<h2 class="gallery-title"><a href="' .
 			  esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?>
 		<p><?php echo get_limited_excerpt(12); ?></p>
 	</div>
@@ -31,7 +45,7 @@
 				<?php echo get_the_author(); ?>
 			</a>
 		</div>
-		<a class="btn btn-double btn-block" 
+		<a class="btn btn-double btn-block"
 		href="<?php echo esc_url( get_permalink($post->ID) ) ?>">
 		<?php echo __('Read More', 'mediatheme') ?></a>
 	</div>
